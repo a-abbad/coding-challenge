557d303df7e9ae9d7b0a23e7c9cb76661fa0b3ae
Abdellwahed ABBAD <abdellwahed.abbad@gmail.com>
2018-01-21 20:36:57 +0000

    add logout method

--- front-end/src/containers/app/index.js
+++ front-end/src/containers/app/index.js
@@ -1,11 +1,20 @@
 import {connect} from 'react-redux';
-import App from './App';
 
-function mapStateToProps({auth}) {
+import App from './App';
+import {logout} from '../../actions/auth';
+
+const mapStateToProps = ({auth}) => {
     const isAuthenticated = true && auth.user;
     return {
-        isAuthenticated
+        isAuthenticated,
+        user: auth.user
     }
-}
+};
 
-export default connect(mapStateToProps, null)(App)
+const mapDispatchToProps = (dispatch) => ({
+    handleLogout : () => {
+        dispatch(logout())
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
