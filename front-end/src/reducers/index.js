40b54eb82224b43e862861248e355be8f5e8c39d
Abdellwahed ABBAD <abdellwahed.abbad@gmail.com>
2018-01-21 20:33:32 +0000

    migrate to react-router-dom

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,11 +1,7 @@
 import {combineReducers} from 'redux';
-import {routerReducer} from 'react-router-redux';
 
-import _test from './_test';
 import auth from './auth';
 
 export default combineReducers({
-    router: routerReducer,
-    _test,
     auth
 });
--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,13 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory'
-import {routerMiddleware} from 'react-router-redux';
 
 import main from '../reducers';
 
-const history = createHistory();
-const historyMiddleware = routerMiddleware(history);
 const loggerMiddleware = createLogger({
     predicate: (state, action) => process.env.NODE_ENV === 'development'
 });
@@ -16,8 +12,7 @@ const store = createStore(
     main,
     applyMiddleware(
         thunkMiddleware,
-        loggerMiddleware,
-        historyMiddleware
+        loggerMiddleware
     )
 );
 
