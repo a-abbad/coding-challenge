679e9e67a1c92901b8e4715fca8f43a8f432597f
Abdellwahed ABBAD <abdellwahed.abbad@gmail.com>
2018-01-21 20:42:30 +0000

    set up host and port of API server

---
+++ front-end/src/config/env.js
@@ -0,0 +1,5 @@
+export default
+{
+  API_HOST: "http://127.0.0.1",
+  API_PORT: "3000"
+}
--- front-end/src/utils/api-utils.js
+++ front-end/src/utils/api-utils.js
@@ -1,6 +1,7 @@
 import 'isomorphic-fetch';
 import jwt_decode from 'jwt-decode';
 import {getData, removeData} from './storage-utils';
+import env from '../config/env'
 
 export function checkStatus(response) {
     if (!response.ok) {
@@ -27,36 +28,40 @@ export function callApi(
     onRequestFailure
 ) {
     return dispatch => {
-        dispatch(request);
+        dispatch(request());
 
-        return fetch(url, config)
+        return fetch(`${env.API_HOST}:${env.API_PORT}` + url, config)
             .then(checkStatus)
             .then(parseJSON)
             .then(json => {
                 dispatch(onRequestSuccess(json));
             })
             .catch(error => {
+
                 const response = error.response;
                 if (response === undefined) {
                     dispatch(onRequestFailure(error));
                 } else {
-                    error.status = response.status;
-                    error.statusText = response.statusText;
-                    response.text().then(text => {
-                        try {
-                            const json = JSON.parse(text);
-                            error.message = json.message;
-                        } catch (ex) {
-                            error.message = text;
-                        }
-                        dispatch(onRequestFailure(error));
+                    let err = {};
+                    err.status = response.status;
+                    err.statusText = response.statusText;
+                    response.text()
+                        .then(text => {
+                            try {
+                                const json = JSON.parse(text);
+                                err.message = json.message;
+                                err.details = json.details;
+                            } catch (ex) {
+                                err.message = text;
+                            }
+                            dispatch(onRequestFailure(err));
                     });
                 }
             });
     };
 }
 
-export const ID_TOKEN = 'id_token';
+export const ID_TOKEN = 'token';
 
 export function decodeUserProfile(idToken) {
     try {
@@ -70,6 +75,7 @@ export function loadUserProfile() {
     try {
         const idToken = getData(ID_TOKEN);
         const userProfile = jwt_decode(idToken);
+        console.log(userProfile)
         const now = new Date().getTime() / 1000; // Date().getTime() returns milliseconds.
         // So divide by 1000 to get seconds
         if (now > userProfile['exp']) {
